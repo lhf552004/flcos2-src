75f762279e980d6805683f8f3db80952d596fc26
Allen Li <allen.li@enisco.com>
2021-07-28 14:26:17 +0800

    angular icon

--- src/app/workflow/workflow.module.ts
+++ src/app/workflow/workflow.module.ts
@@ -4,13 +4,14 @@ import { WorkflowRoutingModule } from './workflow-routing.module';
 import { WorkFlowDesignerComponent } from './work-flow-designer/work-flow-designer.component';
 import { WorkflowsComponent } from './workflows/workflows.component';
 import { WorkflowDefaultComponent } from './workflow-default/workflow-default.component';
-
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [WorkFlowDesignerComponent, WorkflowsComponent, WorkflowDefaultComponent],
   imports: [
     SharedModule,
-    WorkflowRoutingModule
+    WorkflowRoutingModule,
+    MatIconModule
   ]
 })
 export class WorkflowModule { }
