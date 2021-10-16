31a613ad9456a6ac32ddb9765bdcc258190c6d73
Allen Li <allen.li@enisco.com>
2021-10-16 15:12:18 +0800

    SSE

--- flcos-server/src/main/java/com/enisco/flcos/server/api/OPCUAController.java
+++ flcos-server/src/main/java/com/enisco/flcos/server/api/OPCUAController.java
@@ -15,10 +15,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.HtmlUtils;
 
 import java.util.Map;
 import java.util.Set;
